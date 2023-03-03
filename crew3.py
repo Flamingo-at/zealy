1e5d3e5b92f9d46e5289a45e1340fc5a43eb61e9
Flamingo-at <119711235+Flamingo-at@users.noreply.github.com>
2023-03-03 14:37:10 +0200

    Add files via upload

--- crew3.py
+++ crew3.py
@@ -1,25 +1,10 @@
-import asyncio
-import aiohttp
+import time
+import requests
 
 from web3.auto import w3
 from loguru import logger
-from aiohttp import ClientSession
-from random import choice, randint
-from aiohttp_proxy import ProxyConnector
 from eth_account.messages import encode_defunct
 from pyuseragents import random as random_useragent
-
-
-def random_tor_proxy():
-    proxy_auth = str(randint(1, 0x7fffffff)) + ':' + \
-        str(randint(1, 0x7fffffff))
-    proxies = f'socks5://{proxy_auth}@localhost:' + str(9150)
-    return(proxies)
-
-
-def get_connector():
-    connector = ProxyConnector.from_url(random_tor_proxy())
-    return(connector)
 
 
 def create_wallet():
@@ -33,73 +18,69 @@ def create_signature(nonce: str, private_key: str):
     return(signed_message.signature.hex())
 
 
-async def main():
-    for i in range(count):
+def main():
+    for _ in range(count):
         try:
-            async with aiohttp.ClientSession(
-                connector=get_connector(),
-                headers={
-                    'origin': f'https://{name}.crew3.xyz',
-                    'user-agent': random_useragent()}
-            ) as client:
+            with requests.Session() as client:
+                client.headers.update({
+                    'origin': 'https://crew3.xyz',
+                    'user-agent': random_useragent()})
 
                 address, private_key = create_wallet()
 
-                await client.get(f'https://api.crew3.xyz/communities/invitations/{ref}')
-
+                client.get(
+                    f'https://api.crew3.xyz/communities/invitations/{ref}')
                 logger.info('Sending wallet')
-                response = await client.post('https://api.crew3.xyz/authentification/wallet/nonce',
-                                             data={
-                                                 "address": address
-                                             })
-                data = await response.json()
+                response = client.post('https://api.crew3.xyz/authentification/wallet/nonce',
+                                       json={
+                                           "address": address
+                                       })
+                data = response.json()
                 session_id = data['id']
                 nonce = data['nonce']
 
                 logger.info('Verify signature')
                 signature = create_signature(nonce, private_key)
-                response = await client.post(f'https://api.crew3.xyz/authentification/wallet/verify-signature?invitationId={ref}',
-                                             data={
-                                                 "sessionId": session_id,
-                                                 "signature": signature
-                                             })
+                response = client.post(f'https://api.crew3.xyz/authentification/wallet/verify-signature?invitationId={ref}',
+                                       json={
+                                           "sessionId": session_id,
+                                           "signature": signature,
+                                           "network": 1
+                                       })
                 access_token = (response.headers)['Set-Cookie'].split(';')[0]
 
                 client.headers.update({'cookie': access_token})
 
-                await client.patch('https://api.crew3.xyz/users/me',
-                                   json={
-                                       "username": f'{address[:4]}...{address[-4:]}'
-                                   })
+                client.patch('https://api.crew3.xyz/users/me',
+                             json={
+                                 "username": f'{address[:4]}...{address[-4:]}'
+                             })
 
                 logger.info('Completing a task')
                 if task_type == 1:
-                    await client.post(f'https://api.crew3.xyz/communities/{name}/quests/{quest_id}/claim',
-                                      data={
-                                          "questId": quest_id,
-                                          "type": 'none'
-                                      })
+                    client.post(f'https://api.crew3.xyz/communities/{name}/quests/{quest_id}/claim',
+                                data={
+                                    "questId": quest_id,
+                                    "type": 'none'
+                                })
                 else:
-                    await client.post(f'https://api.crew3.xyz/communities/{name}/quests/{quest_id}/claim',
-                                      data={
-                                          "value": "joined",
-                                          "questId": quest_id,
-                                          "type": 'telegram'
-                                      })
+                    client.post(f'https://api.crew3.xyz/communities/{name}/quests/{quest_id}/claim',
+                                data={
+                                    "value": "joined",
+                                    "questId": quest_id,
+                                    "type": 'telegram'
+                                })
         except:
-            logger.error('Error\n')
+            logger.exception('Error\n')
         else:
-            logger.info('Saving data')
             with open(f'{name}_registered.txt', 'a', encoding='utf-8') as f:
                 f.write(f'{address}:{private_key}\n')
             logger.success('Successfully\n')
 
-        await asyncio.sleep(delay)
+        time.sleep(delay)
 
 
 if __name__ == '__main__':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
     print("Bot Crew3 @flamingoat\n")
 
     task_type = input('Task type: daily claim(1) or telegram(2): ')
@@ -109,4 +90,4 @@ if __name__ == '__main__':
     count = int(input('Count: '))
     delay = int(input('Delay(sec): '))
 
-    asyncio.run(main())
+    main()
